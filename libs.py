da966d471013d2ad2d0921b29c93fa0498eb53f9
Dark <108168861+perdark@users.noreply.github.com>
2022-07-27 14:21:09 +0300

    Create libs.py

---
+++ libs.py
@@ -0,0 +1,79 @@
+import imp
+import os
+import random
+import asyncio
+import time
+from telethon.tl import functions, types
+from telethon import events
+import requests
+from telethon.sync import functions, types
+import user_agent
+from user_agent import generate_user_agent
+import requests
+from user_agent import *
+from config import *
+
+abc = 'qwertyuiopasdfghjklzxcvbnm'
+abc2 = 'qwertyuiopasdfghjklzxcvbnm1234567890'
+
+
+@sedthon.on(events.NewMessage(outgoing=True, pattern=".افحصلي (.*)"))
+async def _(event):
+    msg = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 2)
+    for i in range(int(msg[0])):
+        l1 = str(''.join(random.choice(abc)))
+        l2 = str(''.join(random.choice(abc)))
+        l3 = str(''.join(random.choice(abc)))
+        n1 = str(''.join(random.choice(abc2)))
+        n2 = str(''.join(random.choice(abc2)))
+        username = ""
+        ch = str(msg[2])
+        choice = str(msg[1])
+        if choice == "1":
+            username = l1+n1+n1+l1+l1+l1
+
+        if choice == "2":
+            username = l1+'_'+n1+'_'+n2
+
+        if choice == "3":
+            username = l1+n1+l1+n1+l1+n1
+
+        if choice == "4":
+            username = l1+n1+n2+"BOT"
+
+        if choice == "5":
+            username = l1+n1+n1+l2+l3+n2
+
+        url = "https://t.me/"+str(username)
+        headers = {
+            "User-Agent": generate_user_agent(),
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7"}
+        response = requests.get(url, headers=headers)
+        if response.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"') >= 0:
+            await event.client.send_message(event.chat_id, f"Good {username} ✔️")
+            time.sleep(0.5)
+            try:
+                await sedthon(functions.channels.UpdateUsernameRequest(
+                    channel=ch, username=username))
+                await event.client.send_message(event.chat_id, f"Taked {username} ✔️✔️")
+            except:
+                await event.client.send_message(event.chat_id, f"Banned {username} ❌❌")
+        else:
+            await event.edit(f"Bad {username} ❌")
+            time.sleep(0.3)
+    await event.edit("تم الانتهاء من الفحص")
+
+
+@sedthon.on(events.NewMessage(outgoing=True, pattern=".تشيك (.*)"))
+async def _(event):
+    msg = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
+    username = str(msg[0])
+    ch = str(msg[1])
+    try:
+        await sedthon(functions.channels.UpdateUsernameRequest(
+            channel=ch, username=username))
+        await event.client.send_message(event.chat_id, f"Taked {username} ✔️✔️")
+    except:
+        await event.client.send_message(event.chat_id, f"Banned {username} ❌❌")
