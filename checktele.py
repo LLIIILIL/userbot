9d963dd4c4393f5d6eb3cfd7e30a41f5ff749d81
Dark <108168861+perdark@users.noreply.github.com>
2022-08-12 17:51:43 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -2,6 +2,7 @@ import os
 import random
 import asyncio
 import time
+import telethon
 from telethon.tl import functions, types
 from telethon import events
 import requests
@@ -13,8 +14,12 @@ from user_agent import *
 from help import *
 from config import *
 
-a = 'qwertyuiopasdfghjklzxcvbnm'
-b = 'qwertyuiopasdfghjklzxcvbnm1234567890'
+a = 'qwertyuiopassdfghjklzxcvbnm'
+b = '1234567890'
+e = 'qwertyuiopassdfghjklzxcvbnm1234567890'
+x = "X"
+r = "R"
+banned = ["HMMMM"]
 
 
 @sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.تشيكر تلي"))
@@ -22,7 +27,13 @@ async def _(event):
     await event.edit(tele_checker)
 
 
+@sedthon.on(events.NewMessage(outgoing=True, pattern=".اليوزرات المبندة"))
+async def _(event):
+    await event.edit(" , ".join(banned))
+
 # كلايم عدد نوع قناة
+
+
 @sedthon.on(events.NewMessage(outgoing=True, pattern=".كلايم (.*)"))
 async def _(event):
     msg = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 2)
@@ -32,47 +43,72 @@ async def _(event):
         ch = str(msg[2])
         choice = str(msg[1])
         if choice == "1":
-            a = 'qwertyuiopassdfghjklzxcvbnm'
-            b = '1234567890'
             c = d = random.choices(a)
             d = random.choices(b)
             f = [c[0], d[0], c[0], c[0], c[0], d[0]]
             random.shuffle(f)
             username = ''.join(f)
+            if username in banned:
+                c = d = random.choices(a)
+                d = random.choices(b)
+                f = [c[0], d[0], c[0], c[0], c[0], d[0]]
+                random.shuffle(f)
+                username = ''.join(f)
+            else:
+                pass
         if choice == "2":
-            a = 'qwertyuiopassdfghjklzxcvbnm'
-            b = '1234567890'
-            i = 'qwertyuiopassdfghjklzxcvbnm1234567890'
             c = random.choices(a)
             d = random.choices(b)
-            s = random.choices(i)
+            s = random.choices(e)
             f = [c[0], "_", d[0], "_", s[0]]
             username = ''.join(f)
+            if username in banned:
+                c = random.choices(a)
+                d = random.choices(b)
+                s = random.choices(e)
+                f = [c[0], "_", d[0], "_", s[0]]
+                username = ''.join(f)
+            else:
+                pass
         if choice == "3":
-            a = 'qwertyuiopassdfghjklzxcvbnm'
-            b = '1234567890'
             c = d = random.choices(a)
             d = random.choices(b)
             f = [c[0], c[0], c[0], c[0], c[0], d[0]]
             random.shuffle(f)
             username = ''.join(f)
+            if username in banned:
+                c = d = random.choices(a)
+                d = random.choices(b)
+                f = [c[0], c[0], c[0], c[0], c[0], d[0]]
+                random.shuffle(f)
+                username = ''.join(f)
+            else:
+                pass
         if choice == "4":
-            a = 'qwertyuiopassdfghjklzxcvbnm'
-            b = '1234567890'
-            i = 'qwertyuiopassdfghjklzxcvbnm1234567890'
             c = random.choices(a)
             d = random.choices(b)
-            s = random.choices(i)
+            s = random.choices(e)
             f = [c[0], s[0], d[0]]
             random.shuffle(f)
             username = ''.join(f)
             username = username+'bot'
+            if username in banned:
+                c = random.choices(a)
+                d = random.choices(b)
+                s = random.choices(e)
+                f = [c[0], s[0], d[0]]
+                random.shuffle(f)
+                username = ''.join(f)
+                username = username+'bot'
+            else:
+                pass
         url = "https://t.me/"+str(username)
         headers = {
             "User-Agent": generate_user_agent(),
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
             "Accept-Encoding": "gzip, deflate, br",
             "Accept-Language": "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7"}
+        asyncio.sleep(0.3)
         response = requests.get(url, headers=headers)
         if response.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"') >= 0:
             await event.client.send_message(event.chat_id, f"Good {username} ✔️")
@@ -81,9 +117,12 @@ async def _(event):
                 await sedthon(functions.channels.UpdateUsernameRequest(
                     channel=ch, username=username))
                 await event.client.send_message(event.chat_id, f"Taked {username} ✔️✔️")
-                break
-            except:
+            except telethon.errors.rpcerrorlist.UsernameInvalidError:
                 await event.client.send_message(event.chat_id, f"Banned {username} ❌❌")
+                banned.append(username)
+            except:
+                await event.client.send_message(event.chat_id, "Error")
+                break
         else:
             pass
     await event.client.send_message(event.chat_id, "تم الانتهاء من الفحص")
