bb677675604ab373301a405d168b1d689d3e8213
Dark <108168861+perdark@users.noreply.github.com>
2022-08-17 21:49:09 +0300

    Update update.py

--- update.py
+++ update.py
@@ -5,10 +5,39 @@ from sqlalchemy.ext.declarative import declarative_base
 import logging
 from asyncio import CancelledError
 from config import *
-from sqltable import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, PickleType, UnicodeText
+BASE = declarative_base()
+engine = create_engine("sqlite:///sedthon.db", echo=True)
 
 
-Cat_GlobalCollection.__table__.create(checkfirst=True)
+class Cat_GlobalCollection(BASE):
+    __tablename__ = "sed_connection"
+    keywoard = Column(UnicodeText, primary_key=True)
+    contents = Column(PickleType, primary_key=True, nullable=False)
+
+    def __init__(self, keywoard, contents):
+        self.keywoard = keywoard
+        self.contents = tuple(contents)
+
+    def __repr__(self):
+        return "<Cat Global Collection lists '%s' for %s>" % (
+            self.contents,
+            self.keywoard,
+        )
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, Cat_GlobalCollection)
+            and self.keywoard == other.keywoard
+            and self.contents == other.contents
+        )
+
+
+BASE.metadata.create_all(engine)
+
+# Cat_GlobalCollection.__table__.create(checkfirst=True)
 
 CAT_GLOBALCOLLECTION = threading.RLock()
 
@@ -50,13 +79,13 @@ def del_keyword_collectionlist(keywoard):
         SESSION.commit()
 
 
-logger = logging.basicConfig(
+logging.basicConfig(
     format="[%(levelname)s- %(asctime)s]- %(name)s- %(message)s",
     level=logging.INFO,
     datefmt="%H:%M:%S",
 )
 
-LOGS = logger.getLogger(__name__)
+LOGS = logging.getLogger(__name__)
 
 
 @sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.اعادة تشغيل"))
