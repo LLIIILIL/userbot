a3f769237050a72deb2bee1607d5837b00190e70
Dark <108168861+perdark@users.noreply.github.com>
2022-08-17 21:15:46 +0300

    Update sqltable.py

--- sqltable.py
+++ sqltable.py
@@ -1,5 +1,31 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
-Base = declarative_base()
-engine = create_engine("sqlite://", echo=True, future=True)
-Base.metadata.create_all(engine)
+from sqlalchemy import Column, PickleType, UnicodeText
+BASE = declarative_base()
+engine = create_engine("sqlite://sqlalchemy.sqlite", echo=True)
+
+
+class Cat_GlobalCollection(BASE):
+    __tablename__ = "sed_globalcollection"
+    keywoard = Column(UnicodeText, primary_key=True)
+    contents = Column(PickleType, primary_key=True, nullable=False)
+
+    def __init__(self, keywoard, contents):
+        self.keywoard = keywoard
+        self.contents = tuple(contents)
+
+    def __repr__(self):
+        return "<Cat Global Collection lists '%s' for %s>" % (
+            self.contents,
+            self.keywoard,
+        )
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, Cat_GlobalCollection)
+            and self.keywoard == other.keywoard
+            and self.contents == other.contents
+        )
+
+
+BASE.metadata.create_all(engine)
