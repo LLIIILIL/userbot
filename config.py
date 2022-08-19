aada30830c64f166f604b040c74e7ed107815ea6
Dark <108168861+perdark@users.noreply.github.com>
2022-08-19 21:52:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-import os
-import heroku3
+
 APP_ID = os.environ.get("APP_ID")
 APP_HASH = os.environ.get("APP_HASH")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
@@ -10,5 +9,8 @@ SESSION = os.environ.get("TERMUX")
 token = os.environ.get("TOKEN")
 sedthon = TelegramClient(StringSession(session), APP_ID, APP_HASH)
 bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
+UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "master")
 bot.start()
 sedthon.start()
