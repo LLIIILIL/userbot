067e17d7a235d5ed8643143e123067ea6325de26
Dark <108168861+perdark@users.noreply.github.com>
2022-08-19 22:06:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-
+import os
 APP_ID = os.environ.get("APP_ID")
 APP_HASH = os.environ.get("APP_HASH")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
