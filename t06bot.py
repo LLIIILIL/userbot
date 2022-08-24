25d5d6bca0ee46b8151e09c4e99e7e7d3da69841
Dark <108168861+perdark@users.noreply.github.com>
2022-08-24 03:03:57 +0300

    Update t06bot.py

--- t06bot.py
+++ t06bot.py
@@ -22,10 +22,10 @@ async def _(event):
     await sedthon.send_message('@t06bot', '/start')
     asyncio.sleep(2)
     msg0 = await sedthon.get_messages('@t06bot', limit=1)
-    msg0[0].click(2)
+    await msg0[0].click(2)
     asyncio.sleep(2)
     msg1 = await sedthon.get_messages('@t06bot', limit=1)
-    msg1[0].click(0)
+    await msg1[0].click(0)
 
     chs = 1
     for i in range(100):
