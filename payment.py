9de47d87fd4b29371fe9c152179d0dc89ade0bee
Dark <108168861+perdark@users.noreply.github.com>
2022-08-28 18:04:36 +0300

    Update payment.py

--- payment.py
+++ payment.py
@@ -2,23 +2,16 @@ from config import *
 from telethon import events
 from help import *
 
-sedthon.start()
-
-
 @sedthon.on(events.NewMessage(outgoing=True))
 async def _(event):
-    id = event.from_user.id
-    idas = sedthon.get_messages("sedupay", limit=100)
-    o = 0
-    for i in idas:
-        list = str(id[o].message)
-        if id in list and ispay[0] == 'no':
-            ispay.clear()
-            ispay.append("yes")
-        elif id not in list and ispay == 'yes':
-            ispay.clear()
-            ispay.append("no")
-        else:
-            pass
-        o += 1
-sedthon.run_until_disconnected()
+    id = str(event.sender_id)
+    idas = await sedthon.get_messages("sedupay", limit=1)
+    msg = str(idas[0].message)
+    if id in msg and ispay[0] == 'no':
+        ispay.clear()
+        ispay.append("yes")
+    elif id not in msg and ispay[0] == 'yes':
+        ispay.clear()
+        ispay.append("no")
+    else:
+        pass
