243838264847ea531963447c6ac6a78211583d78
b6b65 <113783426+b6b65@users.noreply.github.com>
2022-10-04 04:37:47 +0300

    Update payment.py

--- payment.py
+++ payment.py
@@ -8,7 +8,7 @@ async def _(event):
     id = str(event.sender_id)
     idas = await sedthon.get_messages("sedupay", limit=1)
     msg = str(idas[0].message)
-    if id in msg and ispay[0] == 'no':
+    if id in msg and ispay[0] == 'yes':
         ispay.clear()
         ispay.append("yes")
     elif id not in msg and ispay[0] == 'yes':
@@ -20,11 +20,11 @@ async def _(event):
     id = str(event.sender_id)
     idas = await sedthon.get_messages("sedupay2", limit=1)
     msg = str(idas[0].message)
-    if id in msg and ispay2[0] == 'no':
+    if id in msg and ispay2[0] == 'yes':
         ispay2.clear()
         ispay2.append("yes")
     elif id not in msg and ispay2[0] == 'yes':
         ispay2.clear()
-        ispay2.append("no")
+        ispay2.append("yes")
     else:
         pass
